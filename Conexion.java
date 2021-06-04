0c6e4c7454bd77dcec734d7d28954768f53443e3
Elvin-Morales1996 <80910862+Elvin-Morales1996@users.noreply.github.com>
2021-06-03 21:10:48 -0600

    Add files via upload

---
+++ Conexion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jennifer Portillo
+ */
+public class Conexion {
+    
+    //Conexion a la base de datos proyectofinal
+   
+    private final String URL = "jdbc:mysql://localhost:3306/proyectofinal ?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final String USER = "root";
+    private final String PASSWORD = "elvin";
+    Connection con = null;
+    
+    public Connection getConexion() throws SQLException{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection( URL, USER, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+}
